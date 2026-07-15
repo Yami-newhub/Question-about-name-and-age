35b7019a332d727e55d43fceac1de4488e6206cb
Yami-newhub <vhnewactivity@gmail.com>
2026-07-15 17:10:47 +0700

    first commit

---
+++ alert.js
@@ -0,0 +1,7 @@
+let userName = prompt("Как тебя зовут?");
+    alert(`Привет, ${userName}!`);
+let age = prompt("Сколько тебе лет?");
+    alert(`Тебе ${age} лет.`);
+let consent = confirm("Вы согласны с правилами нашего клуба?");
+    alert(`Твой статус согласия с правилами: ${consent}. Добро пожаловать!`);
+    
